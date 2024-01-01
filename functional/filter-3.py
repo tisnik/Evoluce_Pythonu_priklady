408ec02c852ae643b69ad13aec75053ae8561553
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-01 10:27:50 +0100

    Filter function, 3rd example

---
+++ functional/filter-3.py
@@ -0,0 +1,13 @@
+# Základy funkcionálního programování v Pythonu:
+# - využití standardní funkce vyššího řádu "filter"
+
+# vygenerovat sekvenci celočíselných hodnot
+data = range(0, 11)
+
+# získat pouze liché hodnoty ze sekvence
+filtered = filter(lambda value : value %2 == 1, data)
+print(list(filtered))
+
+# získat pouze sudé hodnoty ze sekvence
+filtered = filter(lambda value : value %2 == 0, data)
+print(list(filtered))
