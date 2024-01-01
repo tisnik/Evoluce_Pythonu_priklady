f1361e0a030791aff9e9ba380cec6284b887bd09
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-01 10:27:50 +0100

    Autocurry transformation

---
+++ functional/autocurry.py
@@ -0,0 +1,30 @@
+# Ukázka autocurryfikace (curryingu) funkce:
+# - založeno na knihovně funcy
+
+from funcy import autocurry
+
+
+def pow(x, y):
+    """Jednoduchá funkce se dvěma parametry."""
+    return x ** y
+
+
+# autocurryfikace funkce pow
+n_pow = autocurry(pow)
+
+# různé způsoby využití objektu n_pow, který jsme získali
+
+# de facto původní funkce (i když interně odlišná)
+powX = n_pow()
+
+# částečně aplikované funkce
+pow2 = n_pow(2)
+pow10 = n_pow(10)
+
+# volání funkce s výsledkem 3^3
+pow3to3 = n_pow(3, 3)
+
+print("powX:", powX(3, 3))
+print("pow2:", pow2(2))
+print("pow10:", pow10(2))
+print("pow3to3:", pow3to3)
