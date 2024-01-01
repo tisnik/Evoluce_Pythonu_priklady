b4f251d72d6f7a2ff7a4743481bc44d27f0d65d6
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-01 10:27:50 +0100

    Filter function, 1st example

---
+++ functional/filter-1.py
@@ -0,0 +1,18 @@
+# Základy funkcionálního programování v Pythonu:
+# - využití standardní funkce vyššího řádu "filter"
+
+# vstupní zpráva
+message = "Lorem ipsum dolor sit amet, consectetur adipiscing " \
+          "elit, sed do eiusmod tempor incididunt ut labore et " \
+          "dolore magna aliqua"
+
+# rozdělení zprávy na jednotlivá slova
+words = message.split()
+
+# získat zprávy, které jsou delší než čtyři znaky
+filtered = filter(lambda word: len(word) > 4, words)
+print(list(filtered))
+
+# získat zprávy, které jsou kratší než pět znaků
+filtered = filter(lambda word: len(word) <= 4, words)
+print(list(filtered))
