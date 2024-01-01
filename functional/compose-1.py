5df54e2aced15f6d8a3438ce21434591a09643b3
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-01 10:27:50 +0100

    Function composition

---
+++ functional/compose-1.py
@@ -0,0 +1,19 @@
+# Kompozice funkcí:
+# - založeno na knihovně funcy
+# - používá se funkce compose
+
+from funcy import compose
+
+
+# kompozice dvou standardních funkcí
+composed = compose(len, str)
+
+# tisk objektu, který jsme získali
+print(composed)
+
+# převod celočíselné hodnoty na řetězec
+# následovaný výpočtem jeho délky
+print(composed(0))
+print(composed(42))
+print(composed(1000))
+print(composed(-1000))
