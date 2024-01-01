bf296525f81b507c844bc46b62440e395dee9ed3
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-01 10:27:50 +0100

    Adder realized as closure

---
+++ functional/closure-adder-1.py
@@ -0,0 +1,26 @@
+# Ukázka uzávěru:
+# - na funkci add() je navázán parametr funkce dummyAdder()
+
+
+def dummyAdder(delta):
+    # lokální funkce, kterou vrátíme
+    def add(n):
+        # přístup k parametru delta předaného vnější funkci
+        return delta + n
+    return add
+
+
+
+# spuštění testů
+def main():
+    adder1 = dummyAdder(0)
+    adder2 = dummyAdder(42)
+    for i in range(1,11):
+        result1 = adder1(i)
+        result2 = adder2(i)
+        print("Iteration #%d" % i)
+        print("    Adder1: %d" % result1)
+        print("    Adder2: %d" % result2)
+
+
+main()
