c5365ba6feacb6eda22c67bc6605133af614abd3
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-01 10:27:50 +0100

    Counter as closure: incorrect

---
+++ functional/counter-closure-1.py
@@ -0,0 +1,34 @@
+# Realizace čítače s využitím uzávěru:
+# - tato varianta je nekorektní
+
+
+def createCounter():
+    # proměnná navázaná na funkci čítače
+    counter = 0
+
+    def next():
+        # pokus o zvýšení nelokální proměnné
+        counter += 1
+        return counter
+
+    # vrácení čítače s navázanou nelokální proměnnou
+    return next
+
+
+
+# spuštění testů čítačů
+def main():
+    # vytvoření dvojice nezávislých čítačů
+    counter1 = createCounter()
+    counter2 = createCounter()
+
+    # použití čítačů - každé jejich zavolání vrátí hodnotu o jedničku vyšší
+    for i in range(1,11):
+        result1 = counter1()
+        result2 = counter2()
+        print("Iteration #%d" % i)
+        print("    Counter1: %d" % result1)
+        print("    Counter2: %d" % result2)
+
+
+main()
