03c0813f53bfb4cedbffced3b9e2c27d7091a51d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-01 10:27:50 +0100

    Function composition

---
+++ functional/compose-2.py
@@ -0,0 +1,23 @@
+# Kompozice funkcí:
+# - založeno na knihovně funcy
+# - používá se funkce compose
+
+from funcy import compose
+
+
+def add(x, y):
+    """Běžná funkce se dvěma argumenty."""
+    return x+y
+
+
+def double(x):
+    """Běžná funkce s jediným argumentem."""
+    return 2*x
+
+
+# kompozice dvou výše uvedených funkcí
+composed = compose(double, add)
+
+# součet následovaný zdvojnásobením hodnoty
+print(composed(2, 3))
+print(composed(-2, -3))
