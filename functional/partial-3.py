46f58c48d499863a5d5f7847694691862bba94a1
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-02 08:40:39 +0100

    Function transformation with 'partial' - example #3

---
+++ functional/partial-3.py
@@ -0,0 +1,26 @@
+# Základní způsob použití funkce `partial` pro transformaci:
+# - funkce se třemi parametry na funkci se dvěma parametry.
+
+
+from functools import partial
+
+
+def mul(x, y, z):
+    """Triviální funkce se třemi parametry."""
+    return x * y * z
+
+
+# ukázka chování původní funkce
+print(mul(2, 3, 7))
+
+print()
+
+# částečná aplikace - výsledkem bude funkce
+# akceptující dva parametry (x je již rovno 2)
+doubler = partial(mul, 2)
+
+
+# korektní způsob volání nové funkce získané transformací
+# - předáváme jí dva parametry
+for i in range(11):
+    print(i, doubler(i, 10))
