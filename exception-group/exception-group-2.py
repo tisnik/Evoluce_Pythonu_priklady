5bf67ecbe8ed374ca54d1d9c6bbf4bcc92cf1d32
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-02 08:38:23 +0100

    Složitější skupina výjimek

---
+++ exception-group/exception-group-2.py
@@ -0,0 +1,27 @@
+# Skupiny výjimek v jazyku Python:
+# - konstrukce skupiny výjimek
+# - tisk skupiny výjimek
+
+import traceback
+
+# konstrukce skupiny výjimek
+eg = ExceptionGroup(
+        "Very serious exception",
+        [
+            TypeError("Unexpected type detected, expecting integer"),
+            ExceptionGroup(
+                "First sub-group",
+                [ValueError("Invalid value"),
+                 FileNotFoundError("Can not find file named foo.bar"),
+                 ZeroDivisionError("Divided by zero")],
+            ),
+            ExceptionGroup(
+                "Second sub-group",
+                [TypeError("Unexpected type detected again, expecting integer"),
+                 ZeroDivisionError("Divided by zero")],
+            ),
+        ],
+)
+
+# tisk skupiny výjimek
+traceback.print_exception(eg)
