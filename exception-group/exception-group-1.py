b6cf89e75bfd760acded4b4bab00610ce2bac077
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-02 08:38:23 +0100

    Vytvoření skupiny výjimek

---
+++ exception-group/exception-group-1.py
@@ -0,0 +1,16 @@
+# Skupiny výjimek v jazyku Python:
+# - vytvoření skupiny výjimek
+# - tisk skupiny výjimek
+
+import traceback
+
+# konstrukce skupiny výjimek
+eg = ExceptionGroup(
+        "Very serious exception",
+        [TypeError("Unexpected type detected, expecting integer"),
+         ValueError("Invalid value"),
+         FileNotFoundError("Can not find file named foo.bar"),
+         ZeroDivisionError("Divided by zero")])
+
+# tisk skupiny výjimek
+traceback.print_exception(eg)
