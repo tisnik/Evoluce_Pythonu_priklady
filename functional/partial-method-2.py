3196fb1368abaca50003643ac1e4f0e3e6f63a8c
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-02 08:40:39 +0100

    Method transformation with 'partialmethod' - example #2

---
+++ functional/partial-method-2.py
@@ -0,0 +1,32 @@
+# Třída, jejíž instance mají atribut _enabled:
+# - metoda set_enabled pro nastavení hodnoty tohoto atributu
+# - speciální metoda __str__ pro získání textové reprezentace
+# - ukázka modifikace atributu přes metodu
+
+
+class Foo:
+    """Třída, jejíž instance mají atribut _enabled."""
+
+    def __init__(self):
+        self._enabled = False
+
+    def set_enabled(self, state):
+        """Nastavení hodnoty atributu _enabled."""
+        self._enabled = state
+
+    def __str__(self):
+        """Získání textové reprezentace objektu."""
+        return "Foo that is " + ("enabled" if self._enabled else "disabled")
+
+
+# vytvoření instance
+foo = Foo()
+print(foo)
+
+# modifikace atributu přes metodu
+foo.set_enabled(True)
+print(foo)
+
+# modifikace atributu přes metodu
+foo.set_enabled(False)
+print(foo)
