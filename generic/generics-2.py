c526d3bae3525cfd093e244eb9840365683300b1
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-03 08:40:47 +0100

    Nový způsob zápisu typových parametrů funkcí, úprava pro starší verze Pythonu

---
+++ generic/generics-2.py
@@ -0,0 +1,12 @@
+# Generické datové typy v Pythonu:
+# - nový způsob zápisu typových parametrů funkcí
+
+from typing import Tuple
+
+
+def pair[T](first: T, second: T) -> Tuple[T, T]:
+    x = (first, second)
+    return x
+
+
+print(pair("A", "B"))
