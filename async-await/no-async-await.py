2d9dff89a068fc5f6ca29f763ad9150c4feafb4e
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-03 08:39:34 +0100

    Non-concurrent tasks

---
+++ async-await/no-async-await.py
@@ -0,0 +1,39 @@
+# Synchronní běh:
+# - vytvoření dvou ynchronních úloh
+# - spuštění úloh (jedna po druhé)
+# - čekání na dokončení obou úloh
+# - doba měření doby trvání běhu obou úloh
+
+from time import sleep, perf_counter
+
+
+# běžná synchronní funkce
+def task(name):
+    print(f"{name} task started")
+    # ynchronní volání
+    sleep(5)
+    print(f"{name} task finished")
+
+
+# běžná synchronní funkce
+def main():
+    # spuštění první úlohy
+    task("first")
+
+    # spuštění druhé úlohy
+    task("second")
+
+    print("done")
+
+
+# měření času
+start = perf_counter()
+
+# přímé spuštění synchronní funkce
+main()
+
+# měření času
+finish = perf_counter()
+
+# výpis dosaženého času
+print(f"Time elapsed: {finish-start : 5.1f}")
