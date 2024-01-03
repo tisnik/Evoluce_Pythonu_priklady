5b6ca4eee99bb3bf98bca4eeacc91fc292cd2ef8
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-03 08:39:34 +0100

    Asyncio usage: incorrect async main invocation

---
+++ async-await/async-await-1.py
@@ -0,0 +1,29 @@
+# Využití standardní knihovny asyncio:
+# - vytvoření asynchronní úlohy
+# - spuštění úlohy
+# - čekání na dokončení úlohy
+# - prozatím nefunkční řešení kvůli způsobu volání main!
+
+import asyncio
+
+
+# funkce, která dokáže dočasně předat řízení přes await
+async def task():
+    print("task started")
+    # asynchronní volání - interpret může vykonávat jinou činnost
+    await asyncio.sleep(5)
+    print("task finished")
+
+
+def main():
+    # jedna z možnosti spuštění úlohy
+    task1 = asyncio.create_task(task())
+    print("task created")
+
+    # (problém) - čekání na dokončení úlohy
+    await task1
+
+    print("done")
+
+
+main()
