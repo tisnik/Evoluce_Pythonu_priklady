23a46369f41e2042dbe1b74aae90acd3b208ee2b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-03 08:40:47 +0100

    Definice funkce se dvěma parametry obecně odlišného typu, úprava pro starší verze Pythonu

---
+++ generic/generics-3.py
@@ -0,0 +1,19 @@
+# Generické datové typy v Pythonu:
+# - definice funkce se dvěma parametry obecně odlišného typu
+# - návratovou hodnotou je dvojice prvků odlišného typu
+
+from typing import TypeVar, Tuple
+
+T = TypeVar('T')
+U = TypeVar('U')
+
+
+def pair(first: T, second: U) -> Tuple[T, U]:
+    x = (first, second)
+    return x
+
+
+reveal_type(pair(1, 2))
+reveal_type(pair(0, "B"))
+reveal_type(pair("A", 42))
+reveal_type(pair("A", "B"))
