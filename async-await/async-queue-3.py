78fc70a880706432bb9988d32dc868fc934e32de
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-03 08:39:34 +0100

    Waiting for five concurrent tasks

---
+++ async-await/async-queue-3.py
@@ -0,0 +1,40 @@
+# Využití standardní knihovny asyncio:
+# - příprava dat pro úlohy
+# - komunikace mezi úlohami přes frontu
+# - souběžné spuštění asynchronních úloh s čekáním na dokončení
+
+import asyncio
+
+
+# vykonání úloh, jejichž parametry jsou přečteny z fronty
+async def task(name, queue):
+    print(f"\tCoroutine #{name} started")
+    while not queue.empty():
+        param = await queue.get()
+        print(f"\t\tTask with parameter {param : 5.2f} started")
+        await asyncio.sleep(5)
+        print(f"\t\tTask with parameter {param : 5.2f} finished")
+    print(f"\tCoroutine #{name} finished")
+
+
+# funkce, která dokáže dočasně předat řízení přes await
+async def main():
+    print("Main started")
+    queue = asyncio.Queue()
+
+    for i in range(10):
+        await queue.put(1/(i+1))
+
+    # spuštění a čekání na dokončení úloh
+    await asyncio.gather(
+        asyncio.create_task(task(1, queue)),
+        asyncio.create_task(task(2, queue)),
+        asyncio.create_task(task(3, queue)),
+        asyncio.create_task(task(4, queue)),
+        asyncio.create_task(task(5, queue)),
+    )
+    print("Main finished")
+
+
+# spuštění funkce, jenž je definována s async
+asyncio.run(main())
