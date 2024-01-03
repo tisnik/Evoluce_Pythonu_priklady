a87cc8c43ae3cf091637277f54bd7ef0266bbaa4
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-03 08:39:34 +0100

    Waiting for tasks, using queue

---
+++ async-await/async-queue-2.py
@@ -0,0 +1,36 @@
+# Využití standardní knihovny asyncio:
+# - příprava dat pro úlohy
+# - komunikace mezi úlohami přes frontu
+# - postupné spuštění asynchronních úloh s čekáním na dokončení
+
+import asyncio
+
+
+# vykonání úloh, jejichž parametry jsou přečteny z fronty
+async def task(name, queue):
+    print(f"\tCoroutine #{name} started")
+    while not queue.empty():
+        param = await queue.get()
+        print(f"\t\tTask with parameter {param : 5.2f} started")
+        await asyncio.sleep(5)
+        print(f"\t\tTask with parameter {param : 5.2f} finished")
+    print(f"\tCoroutine #{name} finished")
+
+
+# funkce, která dokáže dočasně předat řízení přes await
+async def main():
+    print("Main started")
+    queue = asyncio.Queue()
+
+    for i in range(10):
+        await queue.put(1/(i+1))
+
+    for n in range(1, 11):
+        print(f"Starting coroutine #{n}")
+        await asyncio.gather(asyncio.create_task(task(f"{n}", queue)))
+
+    print("Main finished")
+
+
+# spuštění funkce, jenž je definována s async
+asyncio.run(main())
