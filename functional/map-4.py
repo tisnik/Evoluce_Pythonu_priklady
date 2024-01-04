20b3956bdd2df1838fc9ff560a1004fe5aba603b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-04 08:10:54 +0100

    Funkce vyššího řádu map, příklad číslo 4

---
+++ functional/map-4.py
@@ -0,0 +1,14 @@
+# Základy funkcionálního programování v Pythonu:
+# - využití standardní funkce vyššího řádu "map"
+
+# sekvence numerických hodnot
+values = range(-10, 11)
+
+# získání informací o tom, zda jsou prvky kladné, záporné či nulové
+converted = map(
+        lambda x: "negative" if x < 0 else "positive" if x > 0 else "zero",
+        values)
+
+# tisk získaných výsledků na více řádků
+for c in converted:
+    print(c)
