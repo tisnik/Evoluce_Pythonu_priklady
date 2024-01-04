b812be19d9a0146e3eecb3815414a32caa47cce6
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-04 08:11:43 +0100

    Funkce vracející jinou funkci, první příklad

---
+++ functional/get-operator-1.py
@@ -0,0 +1,58 @@
+# Základní použití funkcí vyššího řádu:
+# - funkce vracející jinou funkci
+
+
+def get_operator(symbol):
+    """Na základě předaného symbolu nalezne a vrátí příslušnou funkci."""
+    if symbol == "+":
+        return add
+    elif symbol == "*":
+        return mul
+    elif symbol == "<":
+        return less_than
+    elif symbol == ">":
+        return greater_than
+    else:
+        return None
+
+
+def calc(operator, x, y):
+    """Aplikace předané funkce na parametry x a y."""
+    return operator(x, y)
+
+
+def add(x, y):
+    """Funkce pro součet dvou hodnot."""
+    return x + y
+
+
+def mul(x, y):
+    """Funkce pro součin dvou hodnot."""
+    return x * y
+
+
+def less_than(x, y):
+    """Funkce pro porovnání dvou hodnot na relaci "menší než"."""
+    return x < y
+
+
+def greater_than(x, y):
+    """Funkce pro porovnání dvou hodnot na relaci "větší než"."""
+    return x > y
+
+
+# získat funkci provádějící operaci "+" a následně tuto funkci zavolat
+z = calc(get_operator("+"), 10, 20)
+print(z)
+
+# získat funkci provádějící operaci "*" a následně tuto funkci zavolat
+z = calc(get_operator("*"), 10, 20)
+print(z)
+
+# získat funkci provádějící operaci "<" a následně tuto funkci zavolat
+z = calc(get_operator("<"), 10, 20)
+print(z)
+
+# získat funkci provádějící operaci ">" a následně tuto funkci zavolat
+z = calc(get_operator(">"), 10, 20)
+print(z)
