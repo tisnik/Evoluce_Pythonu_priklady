0051618dc65f14c4729a3d6ab99c3b44b22f75f0
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-04 08:08:47 +0100

    Usage of new keyword: type, 1st example

---
+++ typing/type-1.py
@@ -0,0 +1,18 @@
+# Použití nového klíčového slova type:
+# - deklarace uživatelských datových typů
+
+from typing import List
+
+
+# definice nových typů
+type Names = List[str]
+type Scores = List[int]
+
+
+# použití nově nadefinovaných datových typů
+def print_score_table(names: Names, scores: Scores) -> None:
+    for name, score in zip(names, scores):
+        print(name, score)
+
+
+print_score_table(["aa", "bb"], [1, 2])
