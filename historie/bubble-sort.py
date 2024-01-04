28219f685e43e9e35cc5ecab80f7bbf4850e1e71
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-04 08:13:23 +0100

    Algoritmus bublinkového řazení

---
+++ historie/bubble-sort.py
@@ -0,0 +1,13 @@
+import random
+
+size = 100
+
+a = [random.randrange(0, 10000) for i in range(size)]
+
+for i in range(size - 1, 0, -1):
+    for j in range(0, i):
+        if a[j] > a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
+
+print("Sorted:")
+print(a)
