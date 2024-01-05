7d882d6167a171c31748a9ab0ffa3ae1096a903b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-05 09:15:53 +0100

    Pokus o uložení hodnoty None tam, kde to není dovoleno, úprava pro Python 3.10

---
+++ mypy/dict-type-7B.py
@@ -0,0 +1,13 @@
+# Typové anotace a nástroj Mypy:
+# - definice slovníku
+# - specifikace typu klíčů i typu hodnot
+# - pokus o uložení hodnoty None
+# - úprava pro Python 3.10
+
+d:dict[str, float] = {}
+
+d["foo"] = 1
+d["bar"] = 3.14
+d["baz"] = None
+
+print(d)
