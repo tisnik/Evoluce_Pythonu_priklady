50dc8ed0c59944b5aea6886262c406da82b02992
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-05 09:15:53 +0100

    Specifikace typu klíčů i typu hodnot slovníku

---
+++ mypy/dict-type-3.py
@@ -0,0 +1,14 @@
+# Typové anotace a nástroj Mypy:
+# - definice slovníku
+# - specifikace typu klíčů i typu hodnot
+
+from typing import Dict, Any
+
+d:Dict[Any, Any] = {}
+
+d["foo"] = 1
+d["bar"] = 3.14
+d[10] = 10
+d[42] = "answer"
+
+print(d)
