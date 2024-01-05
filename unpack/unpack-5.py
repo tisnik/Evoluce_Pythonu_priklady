20cb536492ce1f15838e917298e67b3ba3b9b719
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-05 09:04:00 +0100

    Výpis typů hodnot vytvářených s využitím operace unpack

---
+++ unpack/unpack-5.py
@@ -0,0 +1,11 @@
+# Výpis typů hodnot vytvářených s využitím operace unpack
+
+# n-tice, seznam a množina
+x = *range(5), 5
+y = [*range(5), 5]
+z = {*range(5), 5}
+
+# tisk obsahu i typu kolekcí
+print("x:", x, type(x))
+print("y:", y, type(y))
+print("z:", z, type(z))
