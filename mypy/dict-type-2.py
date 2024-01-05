6ef12fc58676115e82fbf6d58b98952d55d1a175
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-05 09:15:53 +0100

    Definice běžného slovníku bez specifikace typů, prvky rozdílných typů

---
+++ mypy/dict-type-2.py
@@ -0,0 +1,12 @@
+# Typové anotace a nástroj Mypy:
+# - definice slovníku
+# - prvky mají rozdílné typy klíčů i hodnot
+
+d = {}
+
+d["foo"] = 1
+d["bar"] = 3.14
+d[10] = 10
+d[42] = "answer"
+
+print(d)
