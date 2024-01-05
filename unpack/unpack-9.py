a8d5e09f586ec045a6cd0b3dacc83f4350cfa48d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-05 09:04:01 +0100

    Zavolání funkce s rozbalením slovníku

---
+++ unpack/unpack-9.py
@@ -0,0 +1,15 @@
+# Zavolání funkce s rozbalením slovníku operací unpack
+
+
+def add3(x, y, z):
+    """Funkce akceptující tři parametry."""
+    print(f"Adding {x} + {y} + {z}")
+    return x+y+z
+
+
+d = {"z": 3,
+     "y": 2,
+     "x": 1}
+
+# rozhalení slovníku
+print(add3(**d))
