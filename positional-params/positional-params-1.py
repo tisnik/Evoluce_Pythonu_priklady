d801021b25162f5cd2940c22094d9ef066c8e472
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-09 09:01:48 +0100

    Definice a volání funkce s pozičními parametry

---
+++ positional-params/positional-params-1.py
@@ -0,0 +1,15 @@
+# Poziční a čistě poziční parametry funkcí:
+# - definice a volání funkce s pozičními parametry
+
+
+# funkce s pozičními parametry
+def foo(x, y, z):
+    return x+y-z
+
+
+# volání funkce s předáním argumentů
+print(foo(1, 2, 10))
+
+# explicitní specifikace parametrů jménem
+# se zachováním jejich pořadí
+print(foo(x=1, y=2, z=10))
