1696cc1ac1eb2d50648811394fbfac487653ab3f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-09 09:02:51 +0100

    Function transformation without decorator, 1st example

---
+++ decorators/no-decorators-1.py
@@ -0,0 +1,28 @@
+# Transformace funkce bez využití dekorátorů:
+# - obalení funkce voláním dalších příkazů
+
+def wrap_function(function):
+    """Transformace předané funkce."""
+
+    def wrapper():
+        """Nová funkce volající předanou funkci."""
+        print("-" * 40)
+        function()
+        print("-" * 40)
+
+    # vrátit novou funkci
+    return wrapper
+
+
+def hello():
+    print("Hello!")
+
+
+# transformace (obalení) funkce hello
+wrapped_hello = wrap_function(hello)
+
+print("Original hello:\n")
+hello()
+
+print("\n\nWrapped hello:\n")
+wrapped_hello()
