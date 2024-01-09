59759e5da4482e59e9c05af6ad9fa3f7c82dcb96
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-09 09:02:51 +0100

    LRU cache: clear cache

---
+++ decorators/lru-cache-4.py
@@ -0,0 +1,32 @@
+# Rekurzivní výpočet Fibonacciho posloupnosti:
+# - verze s LRU cache
+# - průběžné vymazání LRU cache
+
+from time import perf_counter
+from functools import lru_cache
+
+
+@lru_cache
+def fib(n):
+    """Rekurzivní výpočet Fibonacciho posloupnosti."""
+    if n < 2:
+        return n
+    return fib(n-1) + fib(n-2)
+
+
+max_n = 40
+
+# opakovaný výpočet
+for i in range(15):
+    # vymazání cache
+    if i % 5 == 0:
+        fib.cache_clear()
+    print(fib.cache_info())
+    start = perf_counter()
+    result = fib(max_n)
+    end = perf_counter()
+
+    # tisk vypočteného výsledku i doby výpočtu
+    print(result, end - start)
+
+print(fib.cache_info())
