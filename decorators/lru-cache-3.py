ab32401db74e4d588ceb0e364dea5c759269f331
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-09 09:02:51 +0100

    LRU cache: perf timing

---
+++ decorators/lru-cache-3.py
@@ -0,0 +1,30 @@
+# Rekurzivní výpočet Fibonacciho posloupnosti:
+# - verze s LRU cache
+# - výpis informací o využití LRU cache
+
+from time import perf_counter
+from functools import lru_cache
+
+
+@lru_cache
+def fib(n):
+    """Rekurzivní výpočet Fibonacciho posloupnosti."""
+    if n < 2:
+        return n
+    return fib(n-1) + fib(n-2)
+
+
+max_n = 40
+
+# opakovaný výpočet
+for _ in range(10):
+    # tisk informace o využití cache
+    print(fib.cache_info())
+    start = perf_counter()
+    result = fib(max_n)
+    end = perf_counter()
+
+    # tisk vypočteného výsledku i doby výpočtu
+    print(result, end - start)
+
+print(fib.cache_info())
