15f5159011b8a63d1caf72e59ac3433393f25cfd
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-10 08:46:26 +0100

    Optimalizované volání Ackermannovy funkce pro každou dvojici parametrů.

---
+++ walrus-operator/walrus-list-comprehension-4.py
@@ -0,0 +1,28 @@
+# Mroží operátor v Pythonu:
+# - optimalizované volání Ackermannovy funkce
+#   pro každou dvojici parametrů.
+
+from time import perf_counter
+
+
+def A(m, n):
+    """Ackermannova funkce."""
+    if m == 0:
+        return n + 1
+    if n == 0:
+        return A(m - 1, 1)
+    return A(m - 1, A(m, n - 1))
+
+
+# zjistit čas před výpočtem
+start = perf_counter()
+
+results = [result for m in range(4) for n in range(7) if (result := A(m, n)) %2 != 0]
+
+print(results)
+
+# zjistit čas po výpočtu
+finish = perf_counter()
+
+# zobrazení celkové doby výpočtu
+print(f"Duration: {finish-start} seconds")
