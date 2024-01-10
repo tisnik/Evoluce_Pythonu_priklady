f103bad80de805709e5e5db14700474749b80b72
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-10 08:46:32 +0100

    Dvojí volání funkcí len() a sum()

---
+++ walrus-operator/walrus-operator-4.py
@@ -0,0 +1,12 @@
+# Mroží operátor v Pythonu:
+# - dvojí volání funkcí len() a sum()
+
+values = (1, 2, 3, 4, 5)
+
+result = {
+        "count": len(values),
+        "sum": sum(values),
+        "mean": sum(values) / len(values),
+        }
+
+print(result)
