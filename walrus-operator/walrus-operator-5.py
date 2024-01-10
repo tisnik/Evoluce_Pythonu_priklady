0be287dadfdf3a09f32809ce450fb90dd5d8e091
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-10 08:46:32 +0100

    Volání funkcí len() a sum() pouze jedenkrát

---
+++ walrus-operator/walrus-operator-5.py
@@ -0,0 +1,16 @@
+# Mroží operátor v Pythonu:
+# - volání funkcí len() a sum() pouze jedenkrát,
+# - použití pomocných proměnných na globální úrovni
+
+values = (1, 2, 3, 4, 5)
+
+count = len(values)
+summ = sum(values)
+
+result = {
+        "count": count,
+        "sum": summ,
+        "mean": summ/count,
+        }
+
+print(result)
