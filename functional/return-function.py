8c7c69fbc965c3e47627d4a88735a120763933ab
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-11 08:09:02 +0100

    Funkce jako plnohodnotný datový typ

---
+++ functional/return-function.py
@@ -0,0 +1,19 @@
+# Základy funkcionálního programování v Pythonu:
+# - funkce jako plnohodnotný datový typ:
+# - vrácení funkce z jiné funkce příkazem `return`
+
+
+def foo():
+    def bar():
+        print("BAR")
+    return bar
+
+
+# zavoláním foo() získáme funkci bar
+x = foo()
+
+# tisk získané hodnoty a jejího typu
+print(x, type(x))
+
+# funkci bar nyní můžeme spustit: bar()
+x()
