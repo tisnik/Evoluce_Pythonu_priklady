8dfe2e91e684a12d251387b30e6005f033a4a090
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-11 08:07:51 +0100

    Kompozice funkcí, funkce rcompose

---
+++ functional/rcompose-1.py
@@ -0,0 +1,19 @@
+# Kompozice funkcí:
+# - založeno na knihovně funcy
+# - používá se funkce rcompose
+
+from funcy import rcompose
+
+
+# kompozice dvou standardních funkcí
+composed = rcompose(str, len)
+
+# tisk objektu, který jsme získali
+print(composed)
+
+# převod celočíselné hodnoty na řetězec
+# následovaný výpočtem jeho délky
+print(composed(0))
+print(composed(42))
+print(composed(1000))
+print(composed(-1000))
