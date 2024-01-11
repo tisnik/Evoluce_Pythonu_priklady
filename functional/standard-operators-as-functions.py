8b78ed3a117ae369e3921791a0dd12d6757bad82
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-11 08:09:02 +0100

    Funkce vracející jinou funkci z balíčku operator

---
+++ functional/standard-operators-as-functions.py
@@ -0,0 +1,44 @@
+# Základy funkcionálního programování v Pythonu:
+# - použití funkcí vyššího řádu
+# - funkce vracející jinou funkci z balíčku operator
+
+
+from operator import add, mul, pow, lt, gt
+
+
+def get_operator(symbol):
+    """Na základě předaného symbolu nalezne a vrátí příslušnou funkci."""
+    operators = {
+            "+": add,
+            "*": mul,
+            "^": pow,
+            "<": lt,
+            ">": gt,
+    }
+    return operators[symbol]
+
+
+def calc(operator, x, y):
+    """Aplikace předané funkce na parametry x a y."""
+    return operator(x, y)
+
+
+# získat funkci provádějící operaci "+" a následně tuto funkci zavolat
+z = calc(get_operator("+"), 10, 20)
+print(z)
+
+# získat funkci provádějící operaci "*" a následně tuto funkci zavolat
+z = calc(get_operator("*"), 10, 20)
+print(z)
+
+# získat funkci provádějící operaci "^" a následně tuto funkci zavolat
+z = calc(get_operator("^"), 10, 20)
+print(z)
+
+# získat funkci provádějící operaci "<" a následně tuto funkci zavolat
+z = calc(get_operator("<"), 10, 20)
+print(z)
+
+# získat funkci provádějící operaci ">" a následně tuto funkci zavolat
+z = calc(get_operator(">"), 10, 20)
+print(z)
