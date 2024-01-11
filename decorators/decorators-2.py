e60a12ec6380f27c6fd9270d7d85e452eb0f3edf
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-11 08:06:57 +0100

    Print function parameters and its return value

---
+++ decorators/decorators-2.py
@@ -0,0 +1,30 @@
+# Transformace funkce s využitím dekorátorů:
+# - tisk parametrů původní funkce
+# - tisk návratové hodnoty původní funkce
+
+def print_params_and_return_val(function):
+    """Transformace předané funkce."""
+
+    def wrapper(x, y):
+        """Nová funkce volající předanou funkci."""
+        print(f"Parameters: x={x} y={y}")
+        value = function(x, y)
+        print(f"Computed value: {value}")
+        return value
+
+    # vrátit novou funkci
+    return wrapper
+
+
+@print_params_and_return_val
+def add(a, b):
+    return a+b
+
+
+# toto již není původní funkce!
+add(1, 2)
+
+print()
+
+# toto již není původní funkce!
+add(3, 4)
