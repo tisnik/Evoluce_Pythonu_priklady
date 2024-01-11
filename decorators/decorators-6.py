cfed0d4bd1879a4fd8051c71b0198abc5ec973e3
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-11 08:06:57 +0100

    Retrieving name of function transformed using decorator

---
+++ decorators/decorators-6.py
@@ -0,0 +1,25 @@
+# Transformace funkce s využitím dekorátoru:
+# - získání jména transformované funkce
+
+def simple_dashes(function):
+    """Transformace předané funkce."""
+
+    def wrapper():
+        """Nová funkce volající předanou funkci."""
+        print("-" * 40)
+        function()
+        print("-" * 40)
+
+    return wrapper
+
+
+@simple_dashes
+def hello():
+    print("Hello!")
+
+
+# zavolat obalenou funkci
+hello()
+
+# tisk jména funkce
+print("function name:", hello.__name__)
