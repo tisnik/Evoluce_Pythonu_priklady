1c357c47f0ee3c3be1f62e6d447344dc81f3ff68
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-12 08:10:49 +0100

    Formátovací řetězce v Pythonu

---
+++ f-strings/f-string-1.py
@@ -0,0 +1,10 @@
+# Formátovací řetězce v Pythonu:
+# - formátování výstupu založené na f-řetězci
+# - dosazení hodnot proměnných do výsledného řetězce
+
+a=1
+b=2
+c=a+b
+
+# výpis obsahu řetězce po doplnění hodnot dle šablony
+print(f"{a}+{b}={c}")
