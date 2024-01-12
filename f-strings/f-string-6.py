8a2a27d3e05f13c52b70ca87f131e74cdb1ee41c
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-12 08:10:49 +0100

    Volání metody v f-řetězci

---
+++ f-strings/f-string-6.py
@@ -0,0 +1,7 @@
+# Formátovací řetězce v Pythonu:
+# - volání metody v f-řetězci
+
+x = "hello world!"
+
+# výpis obsahu řetězce po doplnění hodnot dle šablony
+print(f"Důležitá zpráva pro vás: '{x.capitalize()}'")
