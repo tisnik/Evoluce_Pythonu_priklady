66f55be81da8e496b0fc476805e2bd0a5cdf179b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-12 08:10:49 +0100

    Formátovací řetězce v Pythonu - nekorektní použití

---
+++ f-strings/f-string-2.py
@@ -0,0 +1,10 @@
+# Formátovací řetězce v Pythonu:
+# - chování Pythonu v případě, že se použije běžný řetězec
+#   a nikoli f-řetězec
+
+a=1
+b=2
+c=a+b
+
+# výpis obsahu řetězce po doplnění hodnot dle šablony
+print("{a}+{b}={c}")
