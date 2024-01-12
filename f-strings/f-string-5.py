c2fced9e9f01548ae7c9f9a2c426c9d0cbcc3c29
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-12 08:10:49 +0100

    Volání funkce v f-řetězci

---
+++ f-strings/f-string-5.py
@@ -0,0 +1,7 @@
+# Formátovací řetězce v Pythonu:
+# - volání funkce v f-řetězci
+
+x = "Hello world!"
+
+# výpis obsahu řetězce po doplnění hodnot dle šablony
+print(f"Délka řetězce '{x}' je {len(x)} znaků")
