8b854ae648354f3120b9aeb45cf2af998c9540e2
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-12 08:10:49 +0100

    Výpis tabulky převrácených hodnot - základní varianta

---
+++ f-strings/f-string-9.py
@@ -0,0 +1,12 @@
+# Formátovací řetězce v Pythonu:
+# - výpis tabulky převrácených hodnot - základní varianta
+# - zarovnání hodnot
+# - specifikace šířky hodnot
+
+# výpis obsahu řetězce po doplnění hodnot dle šablony
+print(f"{'x' : >5}{'1/x' : >5}")
+
+for x in range(1, 11):
+    y = 1/x
+    # výpis obsahu řetězce po doplnění hodnot dle šablony
+    print(f"{x : >5} {y : >5}")
