4ba81e2a24940a304996e11afe83bde7d490f054
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-12 08:10:49 +0100

    Výraz s podmínkou použitý v f-řetězci

---
+++ f-strings/f-string-4.py
@@ -0,0 +1,11 @@
+# Formátovací řetězce v Pythonu:
+# - výraz s podmínkou použitý v f-řetězci
+
+a=1
+b=-1
+
+# výpis obsahu řetězce po doplnění hodnot dle šablony
+print(f"Kladné: {'ano' if a>0 else 'ne'}")
+
+# výpis obsahu řetězce po doplnění hodnot dle šablony
+print(f"Kladné: {'ano' if b>0 else 'ne'}")
