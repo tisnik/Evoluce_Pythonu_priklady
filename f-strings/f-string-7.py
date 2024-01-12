546755a76294644109e8e32c65478f77f6590e91
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-12 08:10:49 +0100

    Výpis jména proměnné společně s její hodnotou

---
+++ f-strings/f-string-7.py
@@ -0,0 +1,8 @@
+# Formátovací řetězce v Pythonu:
+# - výpis jména proměnné společně s její hodnotou
+
+name = "Guido"
+surname = "Rossum"
+
+# výpis obsahu řetězce po doplnění hodnot dle šablony
+print(f"{name=} {surname=}")
