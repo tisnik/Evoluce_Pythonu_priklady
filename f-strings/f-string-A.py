eb20ab352b6cb64ef9f3a01534c049a17c55a622
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-12 08:10:49 +0100

    Výpis tabulky převrácených hodnot, odlišný způsob formátování

---
+++ f-strings/f-string-A.py
@@ -0,0 +1,12 @@
+# Formátovací řetězce v Pythonu:
+# - výpis tabulky převrácených hodnot
+# - varianta se šířkami sloupců
+#   a maximálním počtem cifer za desetinnou tečkou
+
+# výpis obsahu řetězce po doplnění hodnot dle šablony
+print(f"{'x' : >5}{'1/x' : >5}")
+
+for x in range(1, 11):
+    y = 1/x
+    # výpis obsahu řetězce po doplnění hodnot dle šablony
+    print(f"{x : 5} {y : 5.2}")
