5391f5c84bca820a69465f30e730634dc5d81015
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-17 09:14:11 +0100

    Definice n-tice s třemi prvky různých typů, zpětně kompatibilní

---
+++ mypy/tuple-type-3.py
@@ -0,0 +1,7 @@
+# Typové anotace a nástroj Mypy:
+# - definice n-tice se čtyřmi prvky různých typů
+# - kompatibilita s Mypy
+
+from typing import Tuple
+
+p: Tuple[int, float, bool, str] = (1, 3.14, True, "Hello")
