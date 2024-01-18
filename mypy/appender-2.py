7eb621fde01bc855e41d23e17581ac68c4fc15f1
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-18 08:41:43 +0100

    Funkce append s typovými anotacemi

---
+++ mypy/appender-2.py
@@ -0,0 +1,7 @@
+# Typové anotace a nástroj Mypy:
+# - funkce append s typovými anotacemi
+# - korektní řešení (z pohledu typového systému)
+
+
+def append(a:str, b:str) -> str:
+    return a.strip()+b.strip()
