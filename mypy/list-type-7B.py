9a03cf14786ef06a51a3f638a9ef9d29fdd95bc3
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-19 08:48:03 +0100

    Definice seznamu s prvky typu int a str, nová syntaxe

---
+++ mypy/list-type-7B.py
@@ -0,0 +1,6 @@
+# Typové anotace a nástroj Mypy:
+# - definice seznamu s prvky typu int a str
+# - inicializace prvků
+# - úprava pro Python 3.10 a vyšší
+
+lst: list[int | str] = [1, "foo", 42, "bar"]
