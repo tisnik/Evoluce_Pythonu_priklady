77781a44fceb222e1c0f78769f6bc2afa8b6bfe9
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-19 08:48:03 +0100

    Pokus o vložení prvku s nekorektní hodnotou, nová syntaxe

---
+++ mypy/list-type-3B.py
@@ -0,0 +1,7 @@
+# Typové anotace a nástroj Mypy:
+# - definice seznamu s prvky typu int
+# - inicializace prvků
+# - pokus o vložení prvku s nekorektní hodnotou
+# - vyžaduje novější verzi Pythonu
+
+lst: list[int] = [1, 2, None]
