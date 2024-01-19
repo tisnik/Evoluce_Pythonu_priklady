9cb18616984712b149c2738afd64a3dc700e62ef
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-19 08:48:03 +0100

    Definice seznamu s prvky typu int a str, zpětně kompatibilní

---
+++ mypy/list-type-7.py
@@ -0,0 +1,7 @@
+# Typové anotace a nástroj Mypy:
+# - definice seznamu s prvky typu int a str
+# - inicializace prvků
+
+from typing import List, Union
+
+lst: List[Union[int,str]] = [1, "foo", 42, "bar"]
