e3814f61c13d3db0aff19d48475f14cb7f8052a2
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-19 08:48:03 +0100

    Přiřazení nové hodnoty kompatibilního typu do proměnné

---
+++ mypy/variable-3.py
@@ -0,0 +1,8 @@
+# Typové anotace a nástroj Mypy:
+# - specifikace typu lokální proměnné
+# - přiřazení nové hodnoty kompatibilního typu do proměnné
+
+
+def funkce() -> int:
+    x: int = 42
+    return x * 2
