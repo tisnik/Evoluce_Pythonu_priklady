1f280425f37389f900993e38ec04cd76319f4a6d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-23 08:39:45 +0100

    Skript bez dokumentačních řetězců

---
+++ ruff/no-docstrings.py
@@ -0,0 +1,24 @@
+class Test:
+
+    def __init__(self, x):
+        self.value = str(x)
+
+    def __str__(self):
+        return self.value
+
+    def __matmul__(self, other):
+        return Test(f"({self.value} @ {other.value})")
+
+
+def test():
+    t1 = Test(1)
+    print("t1:", t1)
+
+    t2 = Test(2)
+    print("t2:", t2)
+
+    t3 = t1 @ t2
+    print("t1 @ t2:", t3)
+
+
+test()
