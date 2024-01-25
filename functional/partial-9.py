39515a6941ce43ad35ce22f55bbb7ed705e0b8e5
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-25 09:40:44 +0100

    Reformatted, more idiomatic names

--- functional/partial-9.py
+++ functional/partial-9.py
@@ -10,7 +10,7 @@ def mul(x=1, y=1, z=1, w=1):
     return x * y * z * w
 
 
-def displayInfo(name, obj):
+def display_info(name, obj):
     """Tisk základních informací o předané hodnotě."""
     print("name:      ", name)
     print("function:  ", obj.func)
@@ -21,39 +21,39 @@ def displayInfo(name, obj):
 
 # toto nelze pro běžnou funkci provést!
 # f1 = mul
-# displayInfo(f1())
+# display_info(f1())
 
 # částečná aplikace - výsledkem bude funkce,
 # do níž již byl dosazen první parametr.
 f2 = partial(mul, x=2)
 
 # tisk informace o vzniklém objektu
-displayInfo("f2", f2)
+display_info("f2", f2)
 
 # částečná aplikace - výsledkem bude nová funkce,
 # do níž již byly dosazen druhý parametr.
 f3 = partial(mul, y=2)
 
 # tisk informace o vzniklém objektu
-displayInfo("f3", f3)
+display_info("f3", f3)
 
 # částečná aplikace - výsledkem bude nová funkce,
 # do níž již byly dosazeny dva parametry.
 f4 = partial(mul, y=2, z=2)
 
 # tisk informace o vzniklém objektu
-displayInfo("f4", f4)
+display_info("f4", f4)
 
 # částečná aplikace - výsledkem bude nová funkce,
 # do níž již byly dosazeny tři parametry.
 f5 = partial(mul, x=2, y=2, z=2)
 
 # tisk informace o vzniklém objektu
-displayInfo("f5", f5)
+display_info("f5", f5)
 
 # částečná aplikace - výsledkem bude nová funkce,
 # do níž již byly dosazeny všechny čtyři parametry.
 f6 = partial(mul, x=2, y=2, z=2, w=2)
 
 # tisk informace o vzniklém objektu
-displayInfo("f6", f6)
+display_info("f6", f6)
