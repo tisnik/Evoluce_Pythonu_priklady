e4db86d6160a601f75391437a6dc0bcf8ba3b24e
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-26 09:07:16 +0100

    Přeformátováno nástrojem Black

--- f-strings/f-string-C.py
+++ f-strings/f-string-C.py
@@ -9,7 +9,7 @@ castka = 1000
 
 for i in range(1, 11):
     urok = i / 10.0
-    za_rok = castka * (1+urok)
-    za_5_let = castka * (1+urok)**5
+    za_rok = castka * (1 + urok)
+    za_5_let = castka * (1 + urok) ** 5
     # výpis obsahu řetězce po doplnění hodnot dle šablony
     print(f"{castka : >5} {urok : 5.0%} {za_rok : >7.0f} {za_5_let : > 7.0f}")
