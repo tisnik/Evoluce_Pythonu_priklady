34af8fd3439ab94f22049a1cec0701eb588623f9
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-26 09:07:16 +0100

    Přeformátováno nástrojem Black

--- f-strings/f-string-1.py
+++ f-strings/f-string-1.py
@@ -2,9 +2,9 @@
 # - formátování výstupu založené na f-řetězci
 # - dosazení hodnot proměnných do výsledného řetězce
 
-a=1
-b=2
-c=a+b
+a = 1
+b = 2
+c = a + b
 
 # výpis obsahu řetězce po doplnění hodnot dle šablony
 print(f"{a}+{b}={c}")
