990cc5bc303e6003cc4e1f28479fe99e9b644258
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-26 09:07:45 +0100

    Přeformátováno nástrojem Black

--- functional/map-4.py
+++ functional/map-4.py
@@ -6,8 +6,8 @@ values = range(-10, 11)
 
 # získání informací o tom, zda jsou prvky kladné, záporné či nulové
 converted = map(
-        lambda x: "negative" if x < 0 else "positive" if x > 0 else "zero",
-        values)
+    lambda x: "negative" if x < 0 else "positive" if x > 0 else "zero", values
+)
 
 # tisk získaných výsledků na více řádků
 for c in converted:
