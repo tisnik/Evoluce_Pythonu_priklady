bc3504ea6cd4d2be079e73c616d07255b66d5d83
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-26 09:07:16 +0100

    Přeformátováno nástrojem Black

--- f-strings/string-format.py
+++ f-strings/string-format.py
@@ -1,8 +1,8 @@
 # Formátování výstupu s využitím metody str.format
 
-a=1
-b=2
-c=a+b
+a = 1
+b = 2
+c = a + b
 
 # výpis obsahu řetězce po doplnění hodnot dle šablony
 print("{}+{}={}".format(a, b, c))
