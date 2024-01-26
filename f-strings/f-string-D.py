21a13a0836703e0204c66c5b75e519551eb46526
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-26 09:07:16 +0100

    Přeformátováno nástrojem Black

--- f-strings/f-string-D.py
+++ f-strings/f-string-D.py
@@ -6,6 +6,6 @@
 # výpis obsahu řetězce po doplnění hodnot dle šablony
 print(f"{'DEC'}  {'BIN' : ^7}  {'OCT' : >3}  {'hex' : >2} {'HEX' : >2} {'ASCII'}")
 
-for i in range(64, 64+32):
+for i in range(64, 64 + 32):
     # výpis obsahu řetězce po doplnění hodnot dle šablony
     print(f"{i : >2d}  {i : 07b}  {i : >3o}  {i : >2x}  {i : >2X}  {i : >c}")
