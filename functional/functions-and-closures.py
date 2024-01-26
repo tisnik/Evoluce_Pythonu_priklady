f4c27f987d3b6050bf33408b955633821815774a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-26 09:07:16 +0100

    Přeformátováno nástrojem Black

--- functional/functions-and-closures.py
+++ functional/functions-and-closures.py
@@ -3,25 +3,24 @@
 
 
 def foo():
+    def bar():
+        """První varianta funkce bar()."""
+        print("original BAR")
 
-   def bar():
-       """První varianta funkce bar()."""
-       print("original BAR")
+    def other_bar():
+        """Druhá varianta funkce bar()."""
+        print("modified BAR")
 
-   def other_bar():
-       """Druhá varianta funkce bar()."""
-       print("modified BAR")
+    def baz(modify):
+        """Modifikace lokální funkce bar."""
+        # budeme modifikovat nelokální proměnnou bar
+        nonlocal bar
+        if modify:
+            bar = other_bar
+        return bar
 
-   def baz(modify):
-       """Modifikace lokální funkce bar."""
-       # budeme modifikovat nelokální proměnnou bar
-       nonlocal bar
-       if modify:
-           bar = other_bar
-       return bar
-
-   # vrátit uzávěr, který nám umožní volat funkci baz
-   return baz
+    # vrátit uzávěr, který nám umožní volat funkci baz
+    return baz
 
 
 x = foo()
