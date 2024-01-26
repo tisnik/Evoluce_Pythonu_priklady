a53d195694399fd774a49572702a168ac4bfd73f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-26 09:07:16 +0100

    Přeformátováno nástrojem Black

--- f-strings/f-string-4.py
+++ f-strings/f-string-4.py
@@ -1,8 +1,8 @@
 # Formátovací řetězce v Pythonu:
 # - výraz s podmínkou použitý v f-řetězci
 
-a=1
-b=-1
+a = 1
+b = -1
 
 # výpis obsahu řetězce po doplnění hodnot dle šablony
 print(f"Kladné: {'ano' if a>0 else 'ne'}")
