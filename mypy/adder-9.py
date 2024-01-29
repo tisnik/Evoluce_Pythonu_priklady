e3e0276b7d961dd4f446724808abb3a9c504004e
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-29 08:22:41 +0100

    Přeformátováno nástrojem Black

--- mypy/adder-9.py
+++ mypy/adder-9.py
@@ -8,16 +8,18 @@ from typing import Union, overload
 
 
 @overload
-def add(a: int, b: int) -> int: ...
+def add(a: int, b: int) -> int:
+    ...
 
 
 @overload
-def add(a: str, b: str) -> str: ...
+def add(a: str, b: str) -> str:
+    ...
 
 
-def add(a:Union[int, str], b:Union[int, str]) -> Union[int, str]:
+def add(a: Union[int, str], b: Union[int, str]) -> Union[int, str]:
     """Funkce s typovými anotacemi."""
-    return a+b
+    return a + b
 
 
 # zavolání funkce add s argumenty různých typů
