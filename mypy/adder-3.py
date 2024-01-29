b339c5da71f3eab3a2ea89111aca0f10c8b4a211
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-29 08:22:41 +0100

    Přeformátováno nástrojem Black

--- mypy/adder-3.py
+++ mypy/adder-3.py
@@ -2,6 +2,6 @@
 # - funkce s plným uvedením typových anotací
 
 
-def add(a:int, b:int) -> int:
+def add(a: int, b: int) -> int:
     """Funkce s typovými anotacemi."""
-    return a+b
+    return a + b
