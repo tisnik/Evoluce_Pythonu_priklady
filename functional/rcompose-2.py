3d1af9452164a02ac03f9affe042c80793c142c8
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-29 08:22:41 +0100

    Přeformátováno nástrojem Black

--- functional/rcompose-2.py
+++ functional/rcompose-2.py
@@ -7,12 +7,12 @@ from funcy import rcompose
 
 def add(x, y):
     """Běžná funkce se dvěma argumenty."""
-    return x+y
+    return x + y
 
 
 def double(x):
     """Běžná funkce s jediným argumentem."""
-    return 2*x
+    return 2 * x
 
 
 # kompozice dvou výše uvedených funkcí
