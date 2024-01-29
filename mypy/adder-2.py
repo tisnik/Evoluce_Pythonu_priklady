0e6faf6a69ad49a2fe7708ebe6af15713b01490b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-29 08:22:41 +0100

    Přeformátováno nástrojem Black

--- mypy/adder-2.py
+++ mypy/adder-2.py
@@ -5,13 +5,13 @@
 
 def add(a, b):
     """Funkce bez typových anotací."""
-    return a+b
+    return a + b
 
 
 # zavolání funkce add s argumenty různých typů
 print(add(1, 2))
 print(add(1.1, 2.2))
-print(add(1+1j, 2+2j))
+print(add(1 + 1j, 2 + 2j))
 print(add("foo", "bar"))
-print(add([1,2,3], [4,5,6]))
-print(add((1,2,3), (4,5,6)))
+print(add([1, 2, 3], [4, 5, 6]))
+print(add((1, 2, 3), (4, 5, 6)))
