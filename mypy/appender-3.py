4c245afbdf886fcc95e803875a38fb94cdc850d4
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-29 08:22:41 +0100

    Přeformátováno nástrojem Black

--- mypy/appender-3.py
+++ mypy/appender-3.py
@@ -3,5 +3,5 @@
 # - buď je chyba v typových anotacích nebo ve volání metody strip
 
 
-def append(a:int, b:int) -> int:
-    return a.strip()+b.strip()
+def append(a: int, b: int) -> int:
+    return a.strip() + b.strip()
