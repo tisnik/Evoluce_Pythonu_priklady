63046fa800d56040de548e43d95b5ca07b7c12a4
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-29 08:22:41 +0100

    Přeformátováno nástrojem Black

--- mypy/callable-3.py
+++ mypy/callable-3.py
@@ -5,14 +5,14 @@
 from typing import Callable
 
 
-def printIsPositive(x:float, condition:Callable[[float], bool]) -> None:
+def printIsPositive(x: float, condition: Callable[[float], bool]) -> None:
     if condition(x):
         print("Positive")
     else:
         print("Negative")
 
 
-def positiveFloat(x:float) -> bool:
+def positiveFloat(x: float) -> bool:
     return x > 0.0
 
 
