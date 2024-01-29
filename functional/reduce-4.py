890b4c11df424e49c0d89a6bb6d0079c4bced62b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-29 08:22:41 +0100

    Přeformátováno nástrojem Black

--- functional/reduce-4.py
+++ functional/reduce-4.py
@@ -6,7 +6,7 @@
 from functools import reduce
 
 # výpočet faktoriálu, interně založený na funkci reduce
-factorial = lambda n: reduce(lambda a, b: a*b, range(1, n+1), 1)
+factorial = lambda n: reduce(lambda a, b: a * b, range(1, n + 1), 1)
 
 
 # výpočet a zobrazení tabulky faktoriálů
