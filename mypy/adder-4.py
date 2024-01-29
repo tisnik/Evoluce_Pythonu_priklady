60c362a47f9083c150fa2140ed5ef4751960c32f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-29 08:22:41 +0100

    Přeformátováno nástrojem Black

--- mypy/adder-4.py
+++ mypy/adder-4.py
@@ -3,15 +3,15 @@
 # - zavolání této funkce pro různé typy argumentů
 
 
-def add(a:int, b:int) -> int:
+def add(a: int, b: int) -> int:
     """Funkce s typovými anotacemi."""
-    return a+b
+    return a + b
 
 
 # zavolání funkce add s argumenty různých typů
 print(add(1, 2))
 print(add(1.1, 2.2))
-print(add(1+1j, 2+2j))
+print(add(1 + 1j, 2 + 2j))
 print(add("foo", "bar"))
-print(add([1,2,3], [4,5,6]))
-print(add((1,2,3), (4,5,6)))
+print(add([1, 2, 3], [4, 5, 6]))
+print(add((1, 2, 3), (4, 5, 6)))
