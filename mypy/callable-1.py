440709b39ae7c5d1bdef7d51e97c6615a7e39020
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-29 08:22:41 +0100

    Přeformátováno nástrojem Black

--- mypy/callable-1.py
+++ mypy/callable-1.py
@@ -3,12 +3,12 @@
 # - lze provést statickou typovou kontrolu
 
 
-def positive(x:float) -> bool:
+def positive(x: float) -> bool:
     return x > 0.0
 
 
 # zavolání s různým typem argumentů
-x:bool = positive(0.5)
-y:int = positive(42)
-z:float = positive(False)
-w:str = positive(3)
+x: bool = positive(0.5)
+y: int = positive(42)
+z: float = positive(False)
+w: str = positive(3)
