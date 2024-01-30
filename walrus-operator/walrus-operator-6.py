05ecd6629a8ccc9f6b292835645b676d6bdc78b8
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-30 08:33:26 +0100

    Přeformátováno nástrojem Black

--- walrus-operator/walrus-operator-6.py
+++ walrus-operator/walrus-operator-6.py
@@ -5,9 +5,9 @@
 values = (1, 2, 3, 4, 5)
 
 result = {
-        "count": (count := len(values)),
-        "sum": (summ := sum(values)),
-        "mean": summ/count,
-        }
+    "count": (count := len(values)),
+    "sum": (summ := sum(values)),
+    "mean": summ / count,
+}
 
 print(result)
