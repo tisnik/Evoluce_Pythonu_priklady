2ccef9bd32cb79bc633563527e059474dce46bfb
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-30 08:33:26 +0100

    Přeformátováno nástrojem Black

--- unpack/unpack-8.py
+++ unpack/unpack-8.py
@@ -5,12 +5,10 @@
 def add3(x, y, z):
     """Funkce akceptující tři parametry."""
     print(f"Adding {x} + {y} + {z}")
-    return x+y+z
+    return x + y + z
 
 
-d = {"x": 1,
-     "y": 2,
-     "z": 3}
+d = {"x": 1, "y": 2, "z": 3}
 
 # rozhalení slovníku
 print(add3(**d))
