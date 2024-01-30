d3b4ee64b5c345c88ce593184ff28391a1746cc6
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-30 08:33:26 +0100

    Přeformátováno nástrojem Black

--- walrus-operator/walrus-operator-4.py
+++ walrus-operator/walrus-operator-4.py
@@ -4,9 +4,9 @@
 values = (1, 2, 3, 4, 5)
 
 result = {
-        "count": len(values),
-        "sum": sum(values),
-        "mean": sum(values) / len(values),
-        }
+    "count": len(values),
+    "sum": sum(values),
+    "mean": sum(values) / len(values),
+}
 
 print(result)
