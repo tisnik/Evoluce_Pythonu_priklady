a94465b026fab0d6f3937ecf0e0c7cc609053fd1
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-30 08:33:26 +0100

    Přeformátováno nástrojem Black

--- walrus-operator/walrus-list-comprehension-5.py
+++ walrus-operator/walrus-list-comprehension-5.py
@@ -2,6 +2,7 @@
 # - lokální proměnná result je viditelná
 #   i po konstrukci seznamu s výsledky.
 
+
 def A(m, n):
     """Ackermannova funkce."""
     if m == 0:
@@ -11,7 +12,7 @@ def A(m, n):
     return A(m - 1, A(m, n - 1))
 
 
-results = [result for m in range(4) for n in range(7) if (result := A(m, n)) %2 != 0]
+results = [result for m in range(4) for n in range(7) if (result := A(m, n)) % 2 != 0]
 
 # zobrazení vypočtených výsledků
 print(results)
